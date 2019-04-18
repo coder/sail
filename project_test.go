adfe2e528ecffbc7f9e380156ed2674bc5940b4b
Nathan Potter <npotter@coder.com>
2019-04-18 15:11:37 +0000

    Add validation tests

---
+++ project_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_project(t *testing.T) {
+	t.Parallel()
+
+	homeDir, err := os.UserHomeDir()
+	require.NoError(t, err)
+
+	conf := mustReadConfig(filepath.Join(metaRoot(), ".sail.toml"))
+
+	var tests = []struct {
+		name            string
+		repo            string
+		expCntName      string
+		expEnsureDirErr bool
+		expCustomBldImg bool
+	}{
+		{
+			"OK",
+			"codercom/bigdur",
+			"codercom_bigdur",
+			false,
+			true,
+		},
+		{
+			"RepoNotExist",
+			"codercom/do-not-exist",
+			"codercom_do-not-exist",
+			true,
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			rb := newRollback()
+			defer rb.run()
+
+			repo, err := ParseRepo(test.repo)
+			require.NoError(t, err)
+
+			p := &project{
+				conf: conf,
+				repo: repo,
+			}
+
+			name := p.cntName()
+			require.Equal(t, test.expCntName, name)
+
+			expLocalDir := filepath.Join(homeDir, "Projects", test.repo)
+
+			require.Equal(t, expLocalDir, p.localDir())
+			require.Equal(t,
+				filepath.Join(expLocalDir, ".sail", "Dockerfile"),
+				p.dockerfilePath(),
+			)
+
+			t.Run("EnsureDir", func(t *testing.T) {
+				err = p.ensureDir()
+				if test.expEnsureDirErr {
+					require.Error(t, err)
+					return
+				}
+
+				require.NoError(t, err)
+				rb.add(func() {
+					err := os.RemoveAll(p.localDir())
+					require.NoError(t, err)
+				})
+			})
+
+			t.Run("BuildImage", func(t *testing.T) {
+				image, isCustom, err := p.buildImage()
+				require.NoError(t, err)
+				if !test.expCustomBldImg {
+					assert.False(t, isCustom)
+					assert.Empty(t, image)
+					return
+				}
+
+				assert.True(t, isCustom)
+
+				labels := requireGetImageLabels(t, image)
+				assertLabel(t, labels, baseImageLabel, p.repo.DockerName())
+
+				rb.add(func() {
+					requireImageRemove(t, p.repo.DockerName())
+				})
+			})
+		})
+	}
+}
--- ubuntu-dev/buildpush.sh
+++ ubuntu-dev/buildpush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -eu
 
-docker build --network=host -t $1 .
+docker build --network=host -t $1 . --label com.coder.sail.base_image=$1
 docker push $1
