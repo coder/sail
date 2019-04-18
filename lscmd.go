06e9a16d11909d8c809de92690080b618554e27a
Ammar Bandukwala <ammar@ammar.io>
2019-04-18 11:08:23 -0500

    Abstract project listing for Kyle

--- lscmd.go
+++ lscmd.go
@@ -11,6 +11,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"go.coder.com/flog"
+	"golang.org/x/xerrors"
 )
 
 type lscmd struct {
@@ -29,7 +30,17 @@ func (c *lscmd) initFlags(fl *flag.FlagSet) {
 	fl.BoolVar(&c.all, "all", false, "Show stopped container.")
 }
 
-func (c *lscmd) handle(gf globalFlags, fl *flag.FlagSet) {
+// projectInfo contains high-level project metadata as returned by the ls
+// command.
+type projectInfo struct {
+	name   string
+	hat    string
+	url    string
+	status string
+}
+
+// listProjects grabs a list of all projects.:
+func listProjects() ([]projectInfo, error) {
 	cli := dockerClient()
 	defer cli.Close()
 
@@ -40,35 +51,48 @@ func (c *lscmd) handle(gf globalFlags, fl *flag.FlagSet) {
 	filter.Add("label", sailLabel)
 
 	cnts, err := cli.ContainerList(ctx, types.ContainerListOptions{
-		All:     c.all,
+		All:     true,
 		Filters: filter,
 	})
 	if err != nil {
-		flog.Fatal("failed to list containers: %v", err)
+		return nil, xerrors.Errorf("failed to list containers: %w", err)
 	}
 
-	tw := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	infos := make([]projectInfo, 0, len(cnts))
 
-	fmt.Fprintf(tw, "name\that\turl\tstatus\n")
 	for _, cnt := range cnts {
-		var name string
+		var info projectInfo
 		if len(cnt.Names) == 0 {
 			// All sail containers should be named.
 			flog.Error("container %v doesn't have a name.", cnt.ID)
 			continue
 		}
-		name = strings.TrimPrefix(cnt.Names[0], "/")
-
-		var (
-			port = cnt.Labels[portLabel]
-			hat  = cnt.Labels[hatLabel]
-		)
+		info.name = strings.TrimPrefix(cnt.Names[0], "/")
 		// Convert the first - into a / in order to produce a
 		// sail-friendly name.
 		// TODO: this is super janky.
-		name = strings.Replace(name, "-", "/", 1)
+		info.name = strings.Replace(info.name, "-", "/", 1)
 
-		fmt.Fprintf(tw, "%v\t%v\thttp://127.0.0.1:%v\t%v\n", name, hat, port, cnt.Status)
+		info.url = "http://127.0.0.1:" + cnt.Labels[portLabel]
+		info.hat = cnt.Labels[hatLabel]
+
+		infos = append(infos, info)
+	}
+
+	return infos, nil
+}
+
+func (c *lscmd) handle(gf globalFlags, fl *flag.FlagSet) {
+	infos, err := listProjects()
+	if err != nil {
+		flog.Fatal("failed to list projects: %v", err)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+
+	fmt.Fprintf(tw, "name\that\turl\tstatus\n")
+	for _, info := range infos {
+		fmt.Fprintf(tw, "%v\t%v\t%v\t%v\n", info.name, info.hat, info.url, info.status)
 	}
 	tw.Flush()
 
