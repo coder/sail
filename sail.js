9b54c19808eb3a5b36b9a25f53fa49ade17a4115
Anmol Sethi <anmol@coder.com>
2019-05-03 12:00:35 -0400

    Add reload UI

--- sail.js
+++ sail.js
@@ -1,16 +1,31 @@
-let reloadInProgress = false
+function startReloadUI() {
+    const div = document.createElement("div")
+    div.className = "msgbox-overlay"
+    div.style.opacity = 1
+    div.style.textAlign = "center"
+    div.innerHTML = `<div class="msgbox">
+<div class="msg">Reloading container</div>
+</div>`
+    document.querySelector(".monaco-workbench").appendChild(div)
+}
+
+function removeElementsByClass(className) {
+    let elements = document.getElementsByClassName(className);
+    for (let e of elements) {
+        e.parentNode.removeChild(e)
+    }
+}
+
+function stopReloadUI() {
+    removeElementsByClass("msgbox-overlay")
+}
+
 let tty
-window.addEventListener("ide-ready", (ev) => {
+window.addEventListener("ide-ready", () => {
     window.ide.workbench.onFileSaved((ev) => {
         if (!ev.endsWith(".sail/Dockerfile")) {
             return
         }
-
-        if (reloadInProgress) {
-            alert("reload is still in progress")
-            return
-        }
-        reloadInProgress = true
 
         const srv = window.ide.workbench.terminalService
 
@@ -24,6 +39,10 @@ window.addEventListener("ide-ready", (ev) => {
         }
         let oldTTY = srv.getActiveInstance()
         srv.setActiveInstance(tty)
+        // Show the panel and focus it to prevent the user from editing the Dockerfile.
+        srv.showPanel(true)
+
+        startReloadUI()
 
         const ws = new WebSocket("ws://" + location.host + "/sail/api/v1/reload")
         ws.onmessage = (ev) => {
@@ -37,7 +56,7 @@ window.addEventListener("ide-ready", (ev) => {
             } else {
                 alert("reload failed; please see logs in sail terminal")
             }
-            reloadInProgress = false
+            stopReloadUI()
         }
     })
 })
