71a299c99a9d3d8203dc4067bd5d83fe84488cd1
Robert Miller <robert@robertm.codes>
2019-05-22 17:43:59 -0400

    Changes based on PR

--- versionmd.go
+++ versionmd.go
@@ -9,21 +9,15 @@ import (
 
 var version string
 
-type versioncmd struct {
-	print bool
-}
+type versioncmd struct {}
 
 func (v *versioncmd) Spec() cli.CommandSpec {
 	return cli.CommandSpec{
 		Name: "version",
-		Desc: fmt.Sprintf("Retrieve the current version"),
+		Desc: fmt.Sprintf("Retrieve the current version."),
 	}
 }
 
-func (v *versioncmd) RegisterFlags(fl *flag.FlagSet) {
-	fl.BoolVar(&v.print, "print", false, "Print the current version of Sail")
-}
-
 func (v *versioncmd) Run(fl *flag.FlagSet) {
-	fmt.Printf("Sail version: %s", version)
+	fmt.Printf(version)
 }
