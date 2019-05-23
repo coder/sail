9207322b2d70c0d344ceddbf5bbaefbedbe1ef69
Nathan Potter <npotter@coder.com>
2019-05-22 20:00:44 -0500

    fixup! Add CI

--- ci/test.sh
+++ ci/test.sh
@@ -2,3 +2,8 @@
 
 set -euxo pipefail
 
+mkdir -p /tmp/sail-code-server-cache
+curl  > /tmp/sail-code-server-cache/code-server https://codesrv-ci.cdr.sh/latest-linux
+chmod +x /tmp/sail-code-server-cache/code-server
+
+go test -v ./...
--- ci/user.sh
+++ ci/user.sh
@@ -7,5 +7,3 @@ sudo groupadd -g 1000 runner
 sudo useradd -u 1000 -g runner -M -G docker -s /bin/bash runner
 
 sudo chown runner:runner -R .
-
-sudo su - runner
