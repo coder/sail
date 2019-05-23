c96ca285415aeb63d4efaa50da8997d4b701ecd3
Nathan Potter <npotter@coder.com>
2019-05-22 19:33:28 -0500

    fixup! Add CI

--- ci/user.sh
+++ ci/user.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 # Add a runner user so we can run docker as 1000:1000.
-sudo adduser -u 1000 -g 1000 -M --disabled-password --gecos "" runner
+sudo adduser --uid 1000 --gid 1000 -M --disabled-password --gecos "" runner
 
 # Add the user to the docker group.
 sudo usermod -aG docker runner
