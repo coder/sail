d91126a83cfb5ab90df3e5f2e6f38ed394862bed
Nathan Potter <npotter@coder.com>
2019-05-22 19:55:11 -0500

    fixup! Add CI

--- ci/user.sh
+++ ci/user.sh
@@ -3,7 +3,8 @@
 set -euxo pipefail
 
 # Add a runner user so we can run docker as 1000:1000.
-sudo useradd -u 1000 -g 1000 -G docker -M  runner
+sudo groupadd -g 1000 runner
+sudo useradd -u 1000 -g runner -M -G docker -s /bin/bash runner
 
 sudo chown runner:runner -R .
 
