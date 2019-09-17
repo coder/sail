c7e8dc11431157c38ac0007d1f3db8ff4b1f5e94
Dean Sheather <dean@deansheather.com>
2019-09-17 12:49:32 +0000

    Add web-ext build steps

--- extension/pack.sh
+++ extension/pack.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-zip -R extension manifest.json out/* logo128.png
+set -e
+
+# Firefox extension (done first because web-ext verifies manifest)
+if [ -z "$AMO_JWT_ISSUER" ]; then
+	web-ext build -i "node_modules/**/*" -i "src/**/*" -i "package.json" -i "tsconfig.json" -i "webpack.config.js" -i "yarn.lock"
+else
+	web-ext sign --api-key="$AMO_JWT_ISSUER" --api-secret="$AMO_JWT_SECRET" -i "node_modules/**/*" -i "src/**/*" -i "package.json" -i "tsconfig.json" -i "webpack.config.js" -i "yarn.lock"
+fi
+
+# Chrome extension
+zip -R chrome-extension.zip manifest.json out/* logo128.png logo.svg
